c8cb1ccbadfc18dad4a31c2acda50eae6c2f3c94
Jacobleakley <jacob.bleakley@gmail.com>
2019-12-01 00:24:14 -0500

    up to exercise 5

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,50 @@
-puts 2
-puts 3
-puts 2 + 3
+# #   EXERCISE 1
+# def double(my_number)
+#     return my_number * 2
+# end 
+# puts double(100)
+
+# #   EXERCISE 2
+# def negative?(num)
+#     if num >= 0 
+#         then puts "FALSE" 
+#     else puts "TRUE"
+#     end 
+# end 
+
+# # EXERCISE 3
+
+# def is_even?(a)
+#     if a % 2 == 0 
+#         then puts "EVEN"
+#     else puts "ODD"
+#     end
+# end
+# is_even?(10)
+# is_even?(9)
+
+# # EXERCISE 4
+
+# def string_tester(string)
+#     if string.length < 8
+#         then puts "FALSE"
+#     else puts "TRUE"
+#     end
+# end
+# string_tester("This is a string")
+# string_tester("strtwo")
+
+# # EXERCISE 5
+
+# def greet_backwards(name)
+#     puts "Hello, #{name.reverse * 2}! Welcome home." 
+# end 
+
+# greet_backwards("Bob")
+# greet_backwards("Shirly")
+# greet_backwards("Sue")
+# greet_backwards("Andy")
+
+#EXERCISE 6D
+
+
